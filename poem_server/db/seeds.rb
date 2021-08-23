e221b4feacac1c161854f45f0057e9a865e5557f
erobbi <elirobbins0@gmail.com>
2021-08-23 13:21:12 -0400

    added relations and seed content

--- poem_server/db/seeds.rb
+++ poem_server/db/seeds.rb
@@ -1,4 +1,15 @@
-puts "🌱 Seeding messages..."
+puts "Deleting movie/actor data..."
+Author.destroy_all
+Poem.destroy_all
+User.destroy_all
 
+puts "🌱 Seeding authors..."
+plath = Author.create(author_name: 'Sylvia Plath')
+
+puts "🌱 Seeding users..."
+daniel = User.create(username: 'Daniel Ezekiel')
+
+puts "🌱 Seeding poems..."
+Poem.create(title: 'Lady Lazarus', poem_content: ' I have done it again.', author_id: plath.id, user_id: daniel.id)
 
 puts "✅ Done seeding!"
