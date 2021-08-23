94a5dbe25779c8366c17844686558f27a8163fb5
William Badger <wkbadger@gmail.com>
2021-08-23 11:29:02 -0500

    basic routes

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="ui menu">
+            <div className="item">
+                <div className="ui primary button">Sign up</div>
+            </div>
+            <div className="item">
+                <div className="ui button">Log-in</div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContainer = () => {
+    return (
+        <div>
+            <h1>Main Container</h1>
+        </div>
+    );
+}
+
+export default MainContainer;
