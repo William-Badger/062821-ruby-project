317e41e3dd7a0e7ce4333a594dc4b8c15714282b
Daniel <dannyblaze469@gmail.com>
2021-08-24 12:15:37 -0400

    Created components for poem_client

---
+++ poem_client/src/components/PoemsContainer.js
@@ -0,0 +1,9 @@
+function PoemContainer() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PoemContainer
---
+++ poem_client/src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBar() {
+    return (
+        <div className="search">
+            
+        </div>
+    );
+}
+
+export default SearchBar;
